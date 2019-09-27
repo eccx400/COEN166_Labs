ded3e7e35a89d839a05ab65c050b6321c64ff0da
eccx400 <echeng@scu.edu>
2019-09-26 17:50:11 -0700

    Add Report

---
+++ Lab1/Part1/Ex1.py
@@ -0,0 +1,14 @@
+a=123+222 # integer addition
+print(a)
+b = 1.5*4 # floating-point multiplication
+print(b)
+c=2**10 # 2 to the power 10
+print(c)
+import math
+print(math.pi)
+print(math.sqrt(36))
+import random
+a=random.random()
+print('a=', a)
+b = random.choice([1,2,3,4])
+print('b=', b)

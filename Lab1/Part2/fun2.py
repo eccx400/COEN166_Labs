643156979a5bf8bfc8a5e05c4e48cb5a8676fcfc
eccx400 <echeng@scu.edu>
2019-09-27 13:36:59 -0700

    Add Ex.9

--- Lab1/Part2/fun2.py
+++ Lab1/Part2/fun2.py
@@ -1,5 +1,5 @@
 def intersect(seq1, seq2):
-	res = []				# Start empty
+	res = []		        # Start empty
 	for x in seq1:			# Scan seq1
 		if x in seq2:		# Common item?
 			res.append(x)	# Add to end

f525a85dd54db1a1526d52319fb9226c53fb046a
eccx400 <echeng@scu.edu>
2019-10-07 16:34:44 -0700

    Finish lab

--- Lab2/agent.py
+++ Lab2/agent.py
@@ -1,12 +1,37 @@
 class VacuumAgent:
-	state = [left, right, current]
+ 	# def __init__(self)
+	#	self.model = { "Left" : None, "Right" : None, "Current" : None}
+	def action(self, state, location, cost):
+		if state[location] == "Dirty":
+			action = "Suck"
+			cost = cost + 1
+		elif location == 0:
+			action = "Right"
+			cost = cost + 1
+		else:
+			action = "Left"
+			cost = cost + 1
+		return action, cost
 
-	def action(current
+	def update(self, action, state, location, cost):
+		if action == "Suck":
+			state[location] == "Clean"
 
-	def result(current, state):
+		elif action == "Right":
+			location = 1
+		else:
+			location = 0
+		return state, location
+
+	def result( state):
 		if state == ["Clean", "Clean"]:
 			return True
 		else:
 			return False
 
-
+	def tester(self, state, location, cost, sequence):
+		if self.result():
+			return sequence
+		action = self.action(state, location, cost)
+		sequence.append(action)
+		state, location = self.update(state, action, location, cost)
--- Lab2/test.py
+++ Lab2/test.py
@@ -1,5 +1,11 @@
-from agent import *
+from agent import VacuumAgent
 import unittest
 
 class Unit_Test(unittest.TestCase):
 
+	def test(self):
+		vac = VacuumAgent()
+		self.assertTrue(vac.tester(["Clean", "Dirty"], 0, 0, []), ["Suck", "Right"])
+
+if __name__  == '__main__':
+	unittest.main()

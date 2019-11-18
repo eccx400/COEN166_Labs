c4112c4549ad4b7b2cadebced8aaa3db80f778f6
eccx400 <echeng@scu.edu>
2019-11-18 12:48:20 -0800

    Add Files

--- Lab5/faceRec.py
+++ Lab5/faceRec.py
@@ -2,12 +2,61 @@
 
 import cv2
 import numpy as np
+import operator
+import os.path
+from collections import Counter
+import glob
+import sys
+import pprint
+
+
+#Get file paths
+s1 = ['att_faces_10/s1/1.pgm','att_faces_10/s1/2.pgm','att_faces_10/s1/3.pgm','att_faces_10/s1/4.pgm','att_faces_10/s1/5.pgm','att_faces_10/s1/6.pgm','att_faces_10/s1/7.pgm','att_faces_10/s1/8.pgm','att_faces_10/s1/9.pgm','att_faces_10/s1/10.pgm']
+s2 = ['att_faces_10/s2/1.pgm','att_faces_10/s2/2.pgm','att_faces_10/s2/3.pgm','att_faces_10/s2/4.pgm','att_faces_10/s2/5.pgm','att_faces_10/s2/6.pgm','att_faces_10/s2/7.pgm','att_faces_10/s2/8.pgm','att_faces_10/s2/9.pgm','att_faces_10/s2/10.pgm']
+s3 = ['att_faces_10/s3/1.pgm','att_faces_10/s3/2.pgm','att_faces_10/s3/3.pgm','att_faces_10/s3/4.pgm','att_faces_10/s3/5.pgm','att_faces_10/s3/6.pgm','att_faces_10/s3/7.pgm','att_faces_10/s3/8.pgm','att_faces_10/s3/9.pgm','att_faces_10/s3/10.pgm']
+s4 = ['att_faces_10/s4/1.pgm','att_faces_10/s4/2.pgm','att_faces_10/s4/3.pgm','att_faces_10/s4/4.pgm','att_faces_10/s4/5.pgm','att_faces_10/s4/6.pgm','att_faces_10/s4/7.pgm','att_faces_10/s4/8.pgm','att_faces_10/s4/9.pgm','att_faces_10/s4/10.pgm']
+s5 = ['att_faces_10/s5/1.pgm','att_faces_10/s5/2.pgm','att_faces_10/s5/3.pgm','att_faces_10/s5/4.pgm','att_faces_10/s5/5.pgm','att_faces_10/s5/6.pgm','att_faces_10/s5/7.pgm','att_faces_10/s5/8.pgm','att_faces_10/s5/9.pgm','att_faces_10/s5/10.pgm']
+s6 = ['att_faces_10/s6/1.pgm','att_faces_10/s6/2.pgm','att_faces_10/s6/3.pgm','att_faces_10/s6/4.pgm','att_faces_10/s6/5.pgm','att_faces_10/s6/6.pgm','att_faces_10/s6/7.pgm','att_faces_10/s6/8.pgm','att_faces_10/s6/9.pgm','att_faces_10/s6/10.pgm']
+s7 = ['att_faces_10/s7/1.pgm','att_faces_10/s7/2.pgm','att_faces_10/s7/3.pgm','att_faces_10/s7/4.pgm','att_faces_10/s7/5.pgm','att_faces_10/s7/6.pgm','att_faces_10/s7/7.pgm','att_faces_10/s7/8.pgm','att_faces_10/s7/9.pgm','att_faces_10/s7/10.pgm']
+s8 = ['att_faces_10/s8/1.pgm','att_faces_10/s8/2.pgm','att_faces_10/s8/3.pgm','att_faces_10/s8/4.pgm','att_faces_10/s8/5.pgm','att_faces_10/s8/6.pgm','att_faces_10/s8/7.pgm','att_faces_10/s8/8.pgm','att_faces_10/s8/9.pgm','att_faces_10/s8/10.pgm']
+s9 = ['att_faces_10/s9/1.pgm','att_faces_10/s9/2.pgm','att_faces_10/s9/3.pgm','att_faces_10/s9/4.pgm','att_faces_10/s9/5.pgm','att_faces_10/s9/6.pgm','att_faces_10/s9/7.pgm','att_faces_10/s9/8.pgm','att_faces_10/s9/9.pgm','att_faces_10/s9/10.pgm']
+s10 = ['att_faces_10/s10/1.pgm','att_faces_10/s10/2.pgm','att_faces_10/s10/3.pgm','att_faces_10/s10/4.pgm','att_faces_10/s10/5.pgm','att_faces_10/s10/6.pgm','att_faces_10/s10/7.pgm','att_faces_10/s10/8.pgm','att_faces_10/s10/9.pgm','att_faces_10/s10/10.pgm']
+
+sall = [s1, s2, s3, s4, s5, s6, s7, s8, s9, s10]
 
 training = []
 testing = []
 
-path = '/home/echeng/Documents/COEN166/Lab5/att_faces_10/att_faces_10/s1/1.pgm'
-img = cv2.imread(path)
+#Add the files to their respective lists
+#Training: 1, 3, 4, 5, 7, 9
+#Test: 2, 6, 8, 10
+for i in sall:
+	for path in i:
+		fileName = os.path.basename(path)
+		picNum = fileName[:-4]
+		if((int(picNum) % 2 != 0) and int(picNum) != 4):
+			# Picture Number 1, 3, 4, 5, 7, 9
+			training.append(path)
+			# print("Training Added " + picNum)
+		else:
+			# Picture Number 2, 6, 8, 10
+			testing.append(path)
+			# print("Test Added " + picNum)
+print("All Files Added")
 
-cv2.imshow('image', img)
-cv2.waitKey(0)
+train = []
+for path in training:
+	img = cv2.imread(path)
+	train.append(img)
+
+
+K = [ 1, 2, 3, 6, 10 ,20 ,30]
+
+#for rank in K:
+	
+##Demo Purposes
+#path = '/home/echeng/Documents/COEN166/Lab5/att_faces_10/att_faces_10/s1/1.pgm'
+#img = cv2.imread(path)
+
+#cv2.imshow('image', img)
+#cv2.waitKey(0)

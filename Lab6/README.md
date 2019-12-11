10e1164eed90700d55bc373d97ed2ec978125b40
eccx400 <41842161+eccx400@users.noreply.github.com>
2019-12-11 12:38:57 -0800

    Add Lab6

---
+++ Lab6/README.md
@@ -0,0 +1,4 @@
+[COEN166 Folder] To setup python3 venv
+
+venv: lab6
+lab6/bin/activate
---
+++ Lab6/Sample.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+train_images = train_images.reshape((60000, 28, 28, 1))
+test_images = test_images.reshape((10000, 28, 28, 1))
+# Normalize pixel values to be between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)) # the 1st 2d-convolutional layer
+# … to be completed by yourself
